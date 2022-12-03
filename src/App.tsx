512a6e39e160959828d207a0c00c43ef4695e34a
eniiku <enikuauth@gmail.com>
2022-12-03 20:13:41 +0100

    feat(App): create basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<div>Navbar</div>}>
+        <Route index element={<div>HomePage</div>} />
+<<<<<<< Updated upstream
+=======
+        <Route path='marketplace' element={<div>marketplace</div>} />
+        <Route path='auctions' element={<div>auctions</div>} />
+        <Route path='drops' element={<div>drops</div>} />
+>>>>>>> Stashed changes
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
