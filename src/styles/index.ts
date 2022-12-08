18414a6d35a69edfc45fcb9f1f7cd2dd925a7682
eniiku <enikuauth@gmail.com>
2022-12-08 17:43:59 +0100

    feat(HomePage): create desktop view for Hero section

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,5 +1,5 @@
 const styles = {
-  xPadding: 'px-3',
+  xPadding: 'px-3 lg:px-24',
 };
 
 export default styles;
--- src/views/Home/HomePage/HomePage.tsx
+++ src/views/Home/HomePage/HomePage.tsx
@@ -7,19 +7,27 @@ const HomePage = () => {
       {/* Hero section */}
 
       <section aria-label='hero section on Home page' className='text-center'>
-        <div className='px-2'>
-          <h1 className='font-clash_display font-medium text-[32px] text-dark_gray_clr-200'>
-            Photography is poetry and beautiful untold stories
+        <div className='px-2 lg:w-11/12 lg:mx-auto'>
+          <h1
+            className='font-clash_display font-medium text-[32px] text-dark_gray_clr-200
+            lg:font-semibold lg:text-[80px]'
+          >
+            Photography is poetry & beautiful untold stories
           </h1>
 
-          <p className='font-satoshi font-medium mt-10 text-dark_gray_clr-300'>
+          <p
+            className='font-satoshi font-medium mt-10 text-dark_gray_clr-300 lg:text-[28px]
+            lg:w-11/12 lg:mx-auto'
+          >
             Flip through more than 10,000 vintage shots, old photograghs,
             historic images and captures seamlessly in one place. Register to
             get top access.
           </p>
         </div>
 
-        <div className='h-[355px] bg-black mt-6 mb-28'>images</div>
+        <div className='h-[355px] bg-black mt-6 mb-28 lg:mt-32 lg:mb-36'>
+          images
+        </div>
       </section>
 
       {/* Featured Products Section */}
