45a5d7a4781acffdfc89f4e2de5f9cf991705094
eniiku <enikuauth@gmail.com>
2022-12-19 22:51:30 +0100

    feat(AuctionPage): create desktop view for page

--- src/views/Auction/AuctionPage/AuctionPage.tsx
+++ src/views/Auction/AuctionPage/AuctionPage.tsx
@@ -6,14 +6,16 @@ import arrowRightIcon from '@/assets/icons/chevron/icon-arrow-right.svg';
 const AuctionPage = () => {
   return (
     <div
-      className={`font-satoshi font-medium text-xl text-dark_gray_clr-200 ${styles.xPadding}`}
+      className={`font-satoshi font-medium text-xl text-dark_gray_clr-200 ${styles.xPadding} lg:text-[28px]`}
     >
       <div className='mt-6'>breadcrumbs</div>
 
       {/* Live Auction */}
 
-      <div className='my-10'>
-        <p>Here’s an overview of products actively on auction, explore!</p>
+      <div className='my-10 lg:my-20'>
+        <p className='lg:text-[28px] lg:mb-24'>
+          Here’s an overview of products actively on auction, explore!
+        </p>
 
         <ul className='flex gap-12 overflow-hidden my-12'>
           {['', '', '', '', '', '', '', ''].map((item, index) => (
@@ -24,10 +26,12 @@ const AuctionPage = () => {
         </ul>
       </div>
 
-      <div className='my-10'>
-        <h3>Top bids from popular creators </h3>
+      <div className='my-10 lg:mb-40'>
+        <h3 className='lg:font-bold lg:text-4xl lg:mb-24'>
+          Top bids from popular creators
+        </h3>
 
-        <ul>
+        <ul className='lg:grid lg:grid-cols-2 lg:gap-20'>
           {['', '', '', '', '', '', '', ''].slice(0, 3).map((item, index) => (
             <li key={index} className='mt-6'>
               <BidsCard />
@@ -36,8 +40,8 @@ const AuctionPage = () => {
         </ul>
       </div>
 
-      <div className='grid place-content-center'>
-        <button className='font-normal flex items-center gap-3'>
+      <div className='grid place-content-center lg:hidden'>
+        <button className='font-normal flex items-center gap-3 mb-6'>
           Load more
           <img src={arrowRightIcon} role='presentation' className='w-[54px]' />
         </button>
