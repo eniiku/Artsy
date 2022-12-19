08d2530c5e5eb0671f9c40074318c730a543050e
eniiku <enikuauth@gmail.com>
2022-12-19 22:29:26 +0100

    feat(LiveCard): create desktop view for comp

--- src/views/Auction/components/LiveCard/LiveCard.tsx
+++ src/views/Auction/components/LiveCard/LiveCard.tsx
@@ -1,12 +1,13 @@
 const LiveCard = () => {
   return (
-    <div className='w-[230px] h-[190px] rounded-2xl bg-purple-700 relative'>
+    <div className='w-[230px] h-[190px] rounded-2xl bg-purple-700 relative lg:w-[490px] lg:h-[400px]'>
       <div
         className=' w-[90%] absolute py-3 bg-[#F5F4F4] bg-opacity-25 rounded-lg
-        bottom-4 left-3 border-[0.5px] border-white backdrop-blur-sm'
+        bottom-4 left-3 border-[0.5px] border-white backdrop-blur-sm lg:bottom-8 lg:left-6
+        lg:py-6'
       >
         {/* TODO: change font family */}
-        <h3 className='text-center font-stix text-xl text-white'>
+        <h3 className='text-center font-stix text-xl text-white lg:text-[40px]'>
           6hr : 40mins : 15s
         </h3>
       </div>
