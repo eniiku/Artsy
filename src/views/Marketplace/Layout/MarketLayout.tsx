22e128094cdb652ab95f97c84be8d3961c3d5fc9
David Enikuomehin <112134400+eniiku@users.noreply.github.com>
2023-02-14 17:33:29 +0100

    fix(Marketplace): fix padding on market layout page

--- src/views/Marketplace/Layout/MarketLayout.tsx
+++ src/views/Marketplace/Layout/MarketLayout.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from 'react-router-dom';
+import styles from '../../../styles';
 
 const MarketLayout = () => {
   return (
-    <div className='mx-2'>
+    <div className={`${styles.xPadding}`}>
       <div className='my-6'>breadcrumbs</div>
 
       <Outlet />
