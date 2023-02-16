ee65259a9bc10aaf52d15f46791e9999362e7b5d
David Enikuomehin <112134400+eniiku@users.noreply.github.com>
2023-02-16 19:27:18 +0100

    feat(Marketplace): create payment page for mobile view

--- src/views/Marketplace/Shop/MarketPayment/MarketPayment.tsx
+++ src/views/Marketplace/Shop/MarketPayment/MarketPayment.tsx
@@ -1,5 +1,24 @@
+import Button from '../components/Button';
+import lockIcon from '@/assets/icons/crypto/icon-lock.svg';
+
 const MarketPayment = () => {
-  return <div>MarketPayment</div>;
+  return (
+    <div>
+      <div className='flex items-center gap-2'>
+        <img src={lockIcon} alt=' ' role='presentation' className='h-5' />
+
+        <h6 className='font-satoshi first-letter:uppercase'>secure server</h6>
+      </div>
+
+      <div className='my-10'>{/* Payment Form */}</div>
+
+      <div className='text-center mb-8'>
+        <Button buttonAction={undefined} type='primary'>
+          Confirm
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default MarketPayment;
